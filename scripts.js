f2ae7f16eb54aae18ff05bec12e8ebf9ac092194
jamila-samadova <semedovac@gmail.com>
2024-11-10 22:12:23 +0400

    add methods

---
+++ scripts.js
@@ -0,0 +1,66 @@
+
+// String
+
+// task1---------
+
+// let pet = "golden retriever";
+// let result = pet.toUpperCase();
+// console.log(result);
+
+// task2----------
+
+// let question = "Can you solve this problem?";
+// let result = question.length;
+// console.log(result);
+
+
+
+// task3----------
+// let activity = "This game is activity";
+// let result = activity.replace("activity", "boring");
+// console.log(result);
+
+// task4------
+// let headline = "Choose Your Adventure !";
+// let result = headline.split(" ")[2];
+// console.log(result);
+
+
+// array
+
+// task5------------------
+// let desserts = ["Ice Cream", "Pie", "Pudding"];
+// let result = desserts.push("Cake");
+
+// let dessertstr = desserts.join(", "); 
+// console.log(dessertstr); 
+
+// task6-------------
+// let cities = ["London", "New York", "Tokyo"];
+// let citiesresult = cities.includes("Paris");
+// console.log (citiesresult);
+
+
+
+// task7----------------------
+// let cars = ["Sedan", "Coupe"];
+// let trucks = ["Pickup", "SUV"];
+
+// let result = cars.concat(trucks);
+// console.log (result);
+
+
+// task8------------------
+// let desserts = ["Ice Cream", "Pie", "Pudding", "Cake"];
+
+// let result= desserts.indexOf("Pie");
+// console.log (result);
+
+
+// bonustask-----------
+
+// let word = "Technology"; 
+// let resultWord = word.split("");
+// let resultWord2 = resultWord.reverse("");
+// let result = resultWord2.join("");
+//  console.log(result);
